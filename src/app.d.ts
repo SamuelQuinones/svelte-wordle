2cb9af7d6ae3035044722dcf3d24e07ef01b91f7
Samuel Quiñones <sdquinones1@gmail.com>
2022-02-17 22:44:43 -0500

    removed unused variable

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,7 +1,6 @@
 /// <reference types="@sveltejs/kit" />
 
 declare const __version__: string;
-declare const __homepage__: string;
 // See https://kit.svelte.dev/docs#typescript
 // for information about these interfaces
 declare namespace App {
