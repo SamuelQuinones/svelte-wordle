0a3eebe7423db4e409492716bf4ae0400cb75142
Samuel Quiñones <sdquinones1@gmail.com>
2022-03-05 17:42:59 -0500

    updated to match newest sveltekit

--- src/app.d.ts
+++ src/app.d.ts
@@ -4,11 +4,8 @@ declare const __version__: string;
 // See https://kit.svelte.dev/docs#typescript
 // for information about these interfaces
 declare namespace App {
-  interface Locals {}
-
-  interface Platform {}
-
-  interface Session {}
-
-  interface Stuff {}
+  // interface Locals {}
+  // interface Platform {}
+  // interface Session {}
+  // interface Stuff {}
 }
