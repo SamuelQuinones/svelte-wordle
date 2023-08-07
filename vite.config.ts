fb8571a2d7fd540047ec54e1f2d49188334bf7b7
Samuel Quiñones <sdquinones1@gmail.com>
2023-08-07 00:11:12 -0400

    more accomodations

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import pkg from './package.json' assert { type: 'json' };
 
 export default defineConfig({
+	preview: { port: 4274 },
 	plugins: [sveltekit()],
 	define: {
 		__VERSION__: `"${pkg.version}"`
