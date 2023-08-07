7a6f546fca56519d6b7f7ad4b5b8f3495ae1d4ed
Samuel Quiñones <sdquinones1@gmail.com>
2023-08-07 00:09:07 -0400

    added darkmode and highcontrast

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+import plugin from 'tailwindcss/plugin';
+
 /** @type {import('tailwindcss').Config} */
 export default {
+	darkMode: 'class',
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [
+		plugin(function ({ addVariant, e }) {
+			addVariant('high-contrast', ({ modifySelectors, separator }) => {
+				modifySelectors(({ className }) => {
+					return `:is(.high-contrast .${e(`high-contrast${separator}${className}`)})`;
+				});
+			});
+		})
+	]
 };
