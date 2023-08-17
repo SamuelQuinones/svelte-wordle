0a0aa27d49807b3a8d1fef167a1ed08cdb8bd7a0
Samuel Quiñones <sdquinones1@gmail.com>
2023-08-17 00:56:58 -0400

    show stats on gameend

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,26 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import { Grid } from '$components/Grid';
 	import { Keyboard } from '$components/Keyboard';
+	import { gameStore } from '$lib/game/stateStore';
 	import type { CharValue } from '$lib/types';
 
 	let currentGuess: CharValue[] = [];
+
+	$: if ($gameStore.playState !== 'playing' && browser) {
+		const statsModal = document.querySelector<HTMLDialogElement>('#svordle-stats-modal');
+		setTimeout(() => statsModal!.showModal(), 2000);
+	}
+
+	$: {
+		if ($gameStore.playState === 'won') {
+			// TODO: Show toast
+			alert('You won!');
+		} else if ($gameStore.playState === 'lost') {
+			// TODO: Show toast
+			alert('You lost');
+		}
+	}
 </script>
 
 <svelte:head>
