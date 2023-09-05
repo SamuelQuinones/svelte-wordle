b5de6d9abb07e8009c49eea8a19be643e6a2e1d1
Samuel Quiñones <sdquinones1@gmail.com>
2023-09-04 21:24:09 -0400

    continued work on transfer logic: - exportable can now share / write to clipboard

--- src/components/Stats/Transfer.svelte
+++ src/components/Stats/Transfer.svelte
@@ -3,9 +3,13 @@
 	import { saveGameStats } from '$lib/localStorage';
 	import type { IGameStats } from '$lib/types';
 	import { statStore } from '$lib/game/statStore';
+	import { svordleShareData, svordleWriteClipboardText } from '$lib/share';
+	import { toastStore } from '$components/Toast';
+	import { tick } from 'svelte';
 
 	let importExportView = 'export';
 	let importTextarea = '';
+	let copied = false;
 
 	$: exportableStats = encrypt(JSON.stringify($statStore));
 	$: importableStats = decrypt(importTextarea);
@@ -32,14 +36,36 @@
 			window.location.reload();
 		}
 	}
+
+	async function onCopied() {
+		const dataToShare = {
+			title: 'Svordle Stats',
+			text: exportableStats
+		};
+
+		const didShare = await svordleShareData(dataToShare);
+		if (didShare) return;
+
+		const didCopy = await svordleWriteClipboardText(dataToShare.text);
+		if (didCopy) {
+			copied = true;
+			await tick();
+			setTimeout(() => (copied = false), 2000);
+			return;
+		}
+
+		toastStore.show({
+			dismissible: false,
+			message: 'Share Operation Incomplete',
+			type: 'error',
+			timeout: 2000
+		});
+	}
 </script>
 
 <div class="flex items-center gap-x-4">
 	{#each ['import', 'export'] as operation}
 		<div class="flex items-center gap-x-1">
-			<label for="{operation}-operation" class="text-sm">
-				{operation}
-			</label>
 			<input
 				id="{operation}-operation"
 				type="radio"
@@ -48,6 +74,9 @@
 				value={operation}
 				bind:group={importExportView}
 			/>
+			<label for="{operation}-operation" class="text-sm">
+				{operation}
+			</label>
 		</div>
 	{/each}
 	<p class="text-sm">my stats</p>
@@ -57,6 +86,7 @@
 		<textarea
 			rows={8}
 			class:bg-red-900={importableStats === null}
+			class:bg-yellow-600={exportableStats === importTextarea}
 			class="block w-full rounded-lg p-3"
 			bind:value={importTextarea}
 		/>
@@ -73,9 +103,29 @@
 		<textarea readonly rows={8} class="block w-full rounded-lg p-3" value={exportableStats} />
 		<div class="mt-4 text-center">
 			<button
-				class="inline-flex items-center rounded-md border border-sky-800 bg-sky-600 px-4 py-2 font-medium text-white transition-colors hover:bg-sky-800 focus:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-offset-2 active:bg-sky-800"
+				on:click={onCopied}
+				class="inline-flex items-center rounded-md border border-sky-600 bg-sky-600 px-4 py-2 font-medium text-white transition-colors hover:border-sky-800 hover:bg-sky-800 focus:border-sky-800 focus:bg-sky-800 focus:outline-none focus:ring-2 focus:ring-offset-2 active:bg-sky-800"
 			>
-				Copy
+				{#if copied}
+					Copied!
+				{:else}
+					Copy <svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="16"
+						height="16"
+						fill="currentColor"
+						style="vertical-align: -.125em; height: 1em; width: 1em;"
+						class="ml-1 inline-block"
+						viewBox="0 0 16 16"
+					>
+						<path
+							d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"
+						/>
+						<path
+							d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"
+						/>
+					</svg>
+				{/if}
 			</button>
 		</div>
 	{/if}
