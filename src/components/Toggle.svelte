adafb1c614e0bbf5bff15447ed8dd2b755b728fb
Samuel Quiñones <sdquinones1@gmail.com>
2023-09-15 14:41:07 -0400

    added styles to toggle

--- src/components/Toggle.svelte
+++ src/components/Toggle.svelte
@@ -3,13 +3,22 @@
 	export let disabled = false;
 </script>
 
-<label class="toggle-label" class:cursor-not-allowed={disabled}>
+<div class="toggle-label flex items-center justify-between">
 	<slot />
 	<input
 		type="checkbox"
-		class="input-toggle"
-		class:cursor-not-allowed={disabled}
+		class="input-toggle cursor-pointer disabled:cursor-not-allowed"
 		bind:checked
 		{disabled}
 	/>
-</label>
+</div>
+
+<style lang="postcss">
+	.input-toggle {
+		@apply relative h-8 w-14 appearance-none rounded-full bg-gray-500 transition-colors checked:bg-green-500 checked:after:translate-x-full disabled:bg-opacity-70 disabled:after:bg-opacity-60;
+	}
+	.input-toggle::after {
+		content: '';
+		@apply absolute left-1 top-1 h-6 w-6 rounded-full bg-white shadow-sm transition-transform;
+	}
+</style>
